da77f487e478689cbe5e350b38501c5a437e5dbd
Trevor Elder <golfertrevor@gmail.com>
2017-10-04 22:05:47 +0000

    added portfolio via resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930003506) do
+ActiveRecord::Schema.define(version: 20171004215137) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
+    t.text     "thumb_image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "skills", force: :cascade do |t|
