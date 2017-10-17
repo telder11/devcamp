c521e746e3b276a5bf7690bbb50ae6de0f8c0758
Trevor Elder <golfertrevor@gmail.com>
2017-10-17 06:43:59 +0000

    created custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,14 @@
 Rails.application.routes.draw do
-  
   resources :portfolios
-  get 'pages/home'
+  
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
-  get 'pages/about'
 
-  get 'pages/contact'
-
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
 
   resources :blogs
+  
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
