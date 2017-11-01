91f04aab99ded2bfa8fa303565b047053fcc7eb2
Trevor Elder <golfertrevor@gmail.com>
2017-11-01 22:40:03 +0000

    added controller concerns for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  before_filter :configure_permitted_parameters, if: :devise_controller?
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
-  
+  include DeviseWhitelist
 end
